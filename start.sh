bc2518fa57c9cb2750a1c17d1f0f56dc910486a0
Mitchell-Johnson <mitchelljoho@gmail.com>
2024-03-20 23:24:51 -0500

    test

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 export EGGWIKI_SETTINGS=$PWD/settings.cfg || bash;
-./venv/bin/uwsgi --http localhost:8080 --master --enable-threads --die-on-term -w eggwiki.server:app
+./venv/bin/uwsgi --http 0.0.0.0:8080 --master --enable-threads --die-on-term -w eggwiki.server:app
