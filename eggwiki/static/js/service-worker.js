ef0711040cd8aa28b3e29992851fdd9c354d83f9
Mitchell-Johnson <mitchelljoho@gmail.com>
2024-03-27 13:33:34 -0700

    push

--- eggwiki/static/js/service-worker.js
+++ eggwiki/static/js/service-worker.js
@@ -1,24 +1,8 @@
-// service-worker.js
+// bungus service-worker.js
 self.addEventListener('install', (event) => {
-	// Cache some resources during the install event
-	event.waitUntil(
-		caches.open('my-cache-name').then((cache) => {
-			return cache.addAll([
-				'eggwiki.js',
-				'../img/otter_512x512.png',
-				'../img/otter_192x192.png',
-				'../img/otter-favicon.ico',
-				// other assets you want to cache
-			])
-		})
-	)
+	// bruh
 })
 
 self.addEventListener('fetch', (event) => {
-	// Respond with cached resources or fetch from the network
-	event.respondWith(
-		caches.match(event.request).then((response) => {
-			return response || fetch(event.request)
-		})
-	)
+	// bruh
 })
